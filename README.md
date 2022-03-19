f5cfcedfea4750dc78495ce881f35c899f84c18c
unknown <ailinseibane@gmail.com>
2022-03-19 13:19:50 -0300

    new branch temp

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Piedra.papel.tijera
-Juego de "Piedra, papel o tijera para jugar en consola de navegador
+Juego de "Piedra, papel o tijera" para jugar en consola de navegador
 Para ejecutar llamar a la funcion jugar();
--- juego.js
+++ juego.js
@@ -1,62 +1,62 @@
 function computerPlay(){
-    n=Math.random()
+    var n=Math.random()
     if (n <(1/3)){
-        computer= 'a'       //'Piedra'
-        console.log( 'Computadora elige Piedra')
+        var computer= 'a';       //'Piedra'
+        console.log( 'Computadora elige Piedra');
     }else if (n>=(1/3) && n<(2/3)){
-        computer = 'b'      //'Papel'
-        console.log( 'Computadora elige Papel')
+        var computer = 'b';      //'Papel'
+        console.log( 'Computadora elige Papel');
     }else{
-        computer= 'c'       //'Tijera'
-        console.log( 'Computadora elige Tijera')
+        var computer= 'c';       //'Tijera'
+        console.log( 'Computadora elige Tijera');
     }
 }
 
 function playerPlay(){
-    option= prompt('Ingrese opcion');
+    var option= prompt('Ingrese opcion');
         
     if (option.toUpperCase()=='PIEDRA'){
-        player='a'
+        var player='a';
         console.log(`La opcion elegida es ${option}`); 
         computerPlay();
         
     }else if(option.toUpperCase()=='PAPEL'){
-        player='b'
+        var player='b';
         console.log(`La opcion elegida es ${option}`);
         computerPlay();
     }else if(option.toUpperCase()=='TIJERA'){
-        player='c'
+        var player='c';
         console.log(`La opcion elegida es ${option}`);
         computerPlay();
     }else{
-        console.log('Error. Ingresar PIEDRA, PAPEL, o TIJERA')
+        console.log('Error. Ingresar PIEDRA, PAPEL, o TIJERA');
         return playerPlay();
         
     }
 }
 
 function jugar(){
-    var final=0
+    var final=0;
     for (i=1;i<=5;i++){
         playerPlay();
-
         if (computer===player){
-        result=0
-        console.log (`Juego ${i}.Empate`)
+            var result=0;
+            console.log (`Juego ${i}.Empate`);
         }else if ((computer==='a' && player=='b') ||(computer==='b' && player=='c') || (computer==='c' && player=='a')){
-        result=1
-        console.log (`Juego ${i}.Gana Jugador`)
+            var result=1;
+            console.log (`Juego ${i}.Gana Jugador`);
         }else{
-        result=-1
-        console.log (`Juego ${i}.Gana computadora`)
+            var result=-1;
+            console.log (`Juego ${i}.Gana computadora`);
         }
-        final=final +result
+        final=final +result;
     }
+    
     if (final>0){
-        return `Puntaje final ${final}. Gana Jugador!!!`
+        return `Puntaje final ${final}. Gana Jugador!!!`;
     }else if(final===0){
-        return `Puntaje final ${final}. Empate`
+        return `Puntaje final ${final}. Empate`;
     }else{
-        return `Puntaje final ${final}. Gana computadora`
+        return `Puntaje final ${final}. Gana computadora`;
     }
 }
